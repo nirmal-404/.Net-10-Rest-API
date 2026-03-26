c00d8845ba1ffdb931de85d1f950d77d35accb6b
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2026-03-26 15:39:56 +0530

    implemented games crud operations

---
+++ GameStore.Api/Dtos/GameDto.cs
@@ -0,0 +1,12 @@
+namespace GameStore.Api.Dtos;
+
+// Dto is a contact between the client and the server since it reporesents
+// a shared agreement about how data will be transfered and used
+
+public record GameDto(
+    int Id,
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+);
